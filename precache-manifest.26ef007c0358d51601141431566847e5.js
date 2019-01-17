1d6fc53b69b6afbd846759fb7de29c4bb44c1ea1
ThePhao <wim@schmitz.cc>
2019-01-17 11:49:50 +0100

    Updates

---
+++ precache-manifest.26ef007c0358d51601141431566847e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d4a0da8683f8e4c88b7",
+    "url": "/SlimsteMensApp/static/js/runtime~main.1d4a0da8.js"
+  },
+  {
+    "revision": "dc9dfdd9a1ec09c7e7bd",
+    "url": "/SlimsteMensApp/static/js/main.dc9dfdd9.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/SlimsteMensApp/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "dc9dfdd9a1ec09c7e7bd",
+    "url": "/SlimsteMensApp/static/css/main.ba24285e.chunk.css"
+  },
+  {
+    "revision": "2d36752fb27867a54dab3989307b2051",
+    "url": "/SlimsteMensApp/index.html"
+  }
+];
